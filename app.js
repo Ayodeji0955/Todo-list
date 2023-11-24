55e3b3cad308422e5dcf3fe080ae32f927e2272f
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2023-11-24 15:07:25 +0100

    added switch statemnet

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
-
 const app = express();
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
     // res.send('Hello');
 
     let today = new Date();
-    let currentDate = today.getDate();
+    let currentDay = today.getDay();
+    let day = "";
 
-    if (currentDate === 6 || currentDate === 0) {
-        res.write(__dirname + "/weekend.html")
-    } else {
-        res.sendFile(__dirname + "/weekday.html");
-    }
-
+   switch (currentDay) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day ="Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+    default:
+        console.log("Error: current day is equal to:" + currentDay);
+   }
+    
+    res.render("list", { kindOfDay: day });
 });
 
 
 app.listen(3000, function() {
     console.log('listening on port 3000')
-})
+}) 
